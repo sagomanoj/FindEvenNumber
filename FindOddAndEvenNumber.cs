9ee530f5082c58292b4f42e1a1882c8bd518ee02
sagomanoj <33486909+sagomanoj@users.noreply.github.com>
2021-09-19 13:04:37 +0530

    Return true if the given number is Even

---
+++ FindOddAndEvenNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class FindOddAndEvenNumber
+    {
+        public bool IsEven(int number)
+        {
+            return (number & 1) == 0;
+        }
+
+    }
+}
